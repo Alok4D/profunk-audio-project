9d4784f547f743ae75eb089826cc95751f4d2b17
Alok Roy <alokroy602701@gmail.com>
2025-11-17 22:26:02 +0600

    Latest Sound Effects fully responsive done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,14 @@
-// const menuToggle = document.getElementById("menuToggle");
-// const navLinks = document.getElementById("navLinks");
-
-// menuToggle.addEventListener("click", () => {
-//   navLinks.classList.toggle("open");
-// });
 const menuToggle = document.getElementById("menuToggle");
 const navLinks = document.getElementById("navLinks");
 
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 
-  // Toggle icon
   if (navLinks.classList.contains("open")) {
-    menuToggle.textContent = "✕"; // Close icon
+    menuToggle.textContent = "✕";
     menuToggle.classList.add("active");
   } else {
-    menuToggle.textContent = "☰"; // Hamburger icon
+    menuToggle.textContent = "☰";
     menuToggle.classList.remove("active");
   }
 });
